98e310add105f65f6a402e82acde3882b48a4e0b
baocvcv <baocvcv@gmail.com>
2019-09-24 18:36:22 +0800

    working on pa1-4

--- PA1/4-jump/main.cpp
+++ PA1/4-jump/main.cpp
@@ -1,11 +1,13 @@
 #include <cstdio>
 #include <cstring>
-#include <iostream>
 
 #define MAX 1e16
 // #define DEBUG
 
-template<typename T> T read(T &x){
+using namespace std;
+
+template <typename T>
+inline T read(T &x){
     T data = 0;
     int sgn = 1;
     char c = getchar();
@@ -23,18 +25,21 @@ template<typename T> T read(T &x){
     return x;
 }
 
-long *t;
-int *l, *r;
-int n;
-
-long long *f;
-long long *q;
-
 int main(){
+    // vars in the problem description
+    long long *t;
+    int *l, *r;
+    int n;
+
+    // f for result
+    long long *f;
+    // q for a queue
+    long long *q;
+
     read(n);
     l = new int[n+1];
     r = new int[n+1];
-    t = new long[n+1];
+    t = new long long[n+1];
     f = new long long[n+1];
 
     for(int i = 1; i < n; i++){
@@ -44,38 +49,48 @@ int main(){
     }
 
     q = new long long[n+1]; // index queue, increasing int [l, r]
-    int ql = 1, qr = 0;
+    int qStart = 1, qEnd = 0;
     f[1] = 0;
 
-    int l_last = 1, r_last = 1;
+    int prevLeftBound= 1, prevRightBound= 1;
     for(int k = 2; k <= n; k++){
-        int l_cur = l_last;
-        int r_cur = r_last;
-        while(r[l_cur] < k && l_cur < k){
-            l_cur++;
+        // move to new section
+        int curLeftBound = prevLeftBound;
+        int curRightBound = prevRightBound;
+        while((k < l[curLeftBound] || k > r[curLeftBound]) && curLeftBound < k){
+            curLeftBound++;
         }
-        while(r[r_cur] < k && r_cur < k){
-            r_cur++;
-        }
-        // check if reachable
-        if(l[l_cur] > k){
+        if(curLeftBound >= k){
+            // unreachable
             f[k] = MAX;
             continue;
         }
 
-        // if reachable
-        while(ql <= qr && q[ql] < l_cur){
-            // pop item
-            ql++;
+        if(curRightBound < curLeftBound){ // fast forward
+            curRightBound = curLeftBound + 1;
         }
-        for(int i = r_last; i <= r_cur; i++){
-            while(ql <= qr && f[q[qr]] > f[r_cur]){
-                qr--;
-            }
-            q[++qr] = i;
+        while(l[curRightBound] <= k && k <= r[curRightBound] && curRightBound < k){
+            curRightBound++; // move to the first point that cannot reach k
         }
 
+        while(qStart <= qEnd && q[qStart] < curLeftBound){
+            // pop item left of curleftbound
+            qStart++;
+        }
+        int i = curLeftBound > prevRightBound ? curLeftBound : prevRightBound;
+        for(; i <= curRightBound; i++){
+            while(qStart <= qEnd && f[q[qEnd]] > f[curRightBound]){
+                qEnd--;
+            }
+            q[++qEnd] = i;
+        }
+
+        // update f
         f[k] = f[q[1]] + t[q[1]];
+
+        // update boundaries
+        prevLeftBound = curLeftBound;
+        prevRightBound = curRightBound;
     }
 
     printf("%lld\n", f[n]);
